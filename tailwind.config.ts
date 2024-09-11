b3f22bdd538db7e6173239f1054d7f0d14058290
Farid Vatani <faridvatani@gmail.com>
2024-09-11 23:53:53 +0330

    chore: Initial folder structure and configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,25 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1200px",
+    },
+    fontFamily: {
+      heading: "Inter, sans-serif",
+      body: "Karla, sans-serif",
+    },
+    container: {
+      center: true,
+      padding: {
+        sm: "1.5rem",
+        md: "2rem",
+        lg: "5.5rem",
       },
     },
+    extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
 export default config;
