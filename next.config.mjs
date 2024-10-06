5ddf0be80e99f818c08e9b526a2bfab36934ef23
Farid Vatani <faridvatani@gmail.com>
2024-10-06 06:53:22 +0330

    chore: Remove export step from GitHub Actions workflow and add next export output in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   // Configure `pageExtensions` to include markdown and MDX files
